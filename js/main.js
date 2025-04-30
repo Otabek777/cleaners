49368a526064796fc7a9a8968696b57863b9ae6f
Otabek <53979293+Otabek777@users.noreply.github.com>
2025-04-30 18:31:44 +0500

    modal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(".asked__btn").click(function() {
     $(this).toggleClass("active");
 });
+$(".open-modal").click(function() {
+    $(".modal").addClass("active");
+    $("body").addClass("hide");
+});
+$(".modal__close").click(function() {
+    $(".modal").removeClass("active");
+    $("body").removeClass("hide");
+});
