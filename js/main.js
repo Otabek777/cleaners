afee1a06520b30b79f4ddf069204c296ed407cf8
Otabek <53979293+Otabek777@users.noreply.github.com>
2025-05-02 00:21:20 +0500

    header fixed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+$(window).scroll(function(){
+    if ( $(this).scrollTop() > 1) {
+        $(".header").addClass("fixed");
+    } else {
+        $(".header").removeClass("fixed");
+    }
+});
 $(".asked__btn").click(function() {
     $(this).toggleClass("active");
 });
